a19a006271b3647763fdfc2882761bd81012a917
Moses He <440030068@gapps.yrdsb.ca>
2025-05-20 20:54:06 -0400

    changed image name

--- Bullets.java
+++ Bullets.java
@@ -6,8 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class Bullets extends Actor
 {
+    
+    public Bullets()
+    {
+        setImage("bulletImage.jpg");
+    }
     /**
      * Act - do whatever the Bullets wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
