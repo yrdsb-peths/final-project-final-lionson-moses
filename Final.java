6f740146162178603a500818f7c1d3ed825be66a
Moses He <440030068@gapps.yrdsb.ca>
2025-05-22 21:11:23 -0400

    gameOver

--- Final.java
+++ Final.java
@@ -17,5 +17,9 @@ public class Final extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        setBackground("images/space.jpg");
+        
+        Label gameOverLabel = new Label("Game Over", 90);
+        addObject(gameOverLabel, 300, 200);
     }
 }
