631533575ffa5891d4a50d2a753081b6187faee5
LionsonM <zulomm@outlook.com>
2025-05-23 08:58:41 +0800

    add energy store

--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World 
 {
     private int bulletCooldown = 0 ;
+    private int energyStore = 0;
     private Jet jet ;
     
     public int score = 0 ;
@@ -46,6 +47,10 @@ public class MyWorld extends World
         if (bulletCooldown  > 0 ) 
         {
             bulletCooldown--;
+        }
+        if(energyStore > 0)
+        {
+            energyStore--;
         }
         
         creatBullets();
@@ -98,7 +103,7 @@ public class MyWorld extends World
             int x = jet.getX() ;
             int y = jet.getY() ;
             addObject(bul , x, y);
-            bulletCooldown = 20 - level ;
+            bulletCooldown = 30 - level ;
         }
     
     //when there's energy, spawn a huge bullet
@@ -111,6 +116,7 @@ public class MyWorld extends World
             energy--;
             elec--;
             setElec();
+            energyStore = 60;
         }
     }
     
