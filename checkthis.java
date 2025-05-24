60d9d4ba0f86a55c25fb1d5bb6e4f9ecb390e214
LionsonM <zulomm@outlook.com>
2025-05-24 10:19:57 +0800

    see this

---
+++ checkthis.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class checkthis here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class checkthis extends Actor
+{
+    /**
+     * Act - do whatever the checkthis wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        //这个陨石出了什么bug？我想把它设计成一个没法被打碎的障碍
+    }
+}
